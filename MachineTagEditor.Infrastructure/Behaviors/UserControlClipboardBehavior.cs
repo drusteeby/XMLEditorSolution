e8c93888d007c67307ab0a05f15ea77647d87e17
drusteeby <drusteeby@gmail.com>
2015-10-08 22:26:54 -0400

    Adding User Control Clipboard Behavior

---
+++ MachineTagEditor.Infrastructure/Behaviors/UserControlClipboardBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace MachineTagEditor.Infrastructure.Behaviors
+{
+    public class UserControlClipboardBehavior : Behavior<UserControl>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            CommandBinding CopyCommandBinding = new CommandBinding(
+                ApplicationCommands.Copy,
+                CopyCommandExecuted,
+                CopyCommandCanExecute);
+            AssociatedObject.CommandBindings.Add(CopyCommandBinding);
+        }
+
+        private void CopyCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        private void CopyCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
